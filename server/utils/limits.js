fe51d42788d2d3de00a962b3f0406bc24ee8e18d
Wantonfury <orzan.alexandru94@gmail.com>
2023-07-17 20:27:43 +0300

    Add limits for post routes

---
+++ server/utils/limits.js
@@ -0,0 +1,20 @@
+exports.username = {
+  min: 3,
+  max: 20
+}
+
+exports.password = {
+  min: 8,
+  max: 100
+}
+
+exports.item = {
+  name: {
+    min: 3,
+    max: 100,
+  },
+  description: {
+    descriptionMin: 3,
+    descriptionMax: 500
+  },
+}
