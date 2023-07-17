fce528562d000ae50c605db8991428ccd62a69c4
Wantonfury <orzan.alexandru94@gmail.com>
2023-07-17 20:21:03 +0300

    Add a basic test navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import '../../styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div id='navbar'>
+      <div className='title'>
+        <span>Odinshop</span>
+      </div>
+      <div className='options'>
+        <span>Options</span>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
