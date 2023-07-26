49a3778991059580aa9d524736fc06a5218adbe0
Wantonfury <orzan.alexandru94@gmail.com>
2023-07-26 20:36:41 +0300

    Add a login/signup option

---
+++ client/src/components/Navbar/NavbarAccountActions.js
@@ -0,0 +1,12 @@
+import '../../styles/Button.css';
+
+const NavbarAccountActions = () => {
+  return (
+    <div className='nav-account-cnt'>
+      <button className='btn'>Sign Up</button>
+      <button className='btn'>Log In</button>
+    </div>
+  );
+}
+
+export default NavbarAccountActions
