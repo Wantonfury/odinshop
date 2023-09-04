b9ee043e3ece2f4ad950a6a00e07a17c3c64348d
Wantonfury <orzan.alexandru94@gmail.com>
2023-09-05 00:33:28 +0300

    Remove previous version

--- server/controllers/itemController.js
+++
@@ -1,9 +0,0 @@
-const Item = require('../models/item');
-
-exports.getItems = (req, res, next) => {
-  Item.find({})
-    .then(items => {
-      res.status(200).json(items);
-    })
-    .catch(err => next(err));
-}
